9c605020ed2a7f5072f452ca2761122fc9244dfc
Aaron Hayden <aaronjhayden@gmail.com>
2020-05-22 00:55:04 +0000

    pytest start

---
+++ simplest.py
@@ -0,0 +1,7 @@
+def test_simplest():
+    assert True
+    
+class TestSimple:
+    
+    def test_simple(self):
+        assert "True string" == "True string"
---
+++ test_data.py
@@ -0,0 +1,16 @@
+from data import is_empty
+
+def test_empty_list():
+    assert is_empty([]) is True
+
+def test_empty_dict():
+    assert is_empty({}) is True
+    
+def test_list_is_not_empty():
+    assert is_empty([1,2,3]) is False
+
+def test_dict_is_not_empty():
+    assert is_empty({"item": 1}) is False
+    
+def test_integer_is_false():
+    assert is_empty(1) is False
